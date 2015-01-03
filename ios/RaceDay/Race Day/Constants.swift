45c0e60ee6b5b80af2024f8a7addc5a81b6f49f9
Dick Fickling <dick@fickling.us>
2015-01-03 11:31:04 -0800

    Add apimanager, arcgis

---
+++ ios/RaceDay/Race Day/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Crouded
+//
+//  Created by Richard Fickling on 12/20/14.
+//  Copyright (c) 2014 App Builders Inc. All rights reserved.
+//
+
+import Foundation
+
+
+let BASE_PATH = "http://crowded9.mybluemix.net"
+
+let navy = UIColor(red: 86/255, green: 109/255, blue: 126/255, alpha: 1.0)
