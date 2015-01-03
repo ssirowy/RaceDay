8ff10983a7b9ff1ed585dd82b5456d0bb67e27d3
Dick Fickling <dick@fickling.us>
2015-01-03 11:11:26 -0800

    Initial iOS commit

---
+++ ios/Race Day/Race Day/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  Race Day
+//
+//  Created by Dick Fickling on 1/3/15.
+//  Copyright (c) 2015 Questionable Intent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
---
+++ ios/Race Day/Race Day/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Race Day
+//
+//  Created by Dick Fickling on 1/3/15.
+//  Copyright (c) 2015 Questionable Intent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
