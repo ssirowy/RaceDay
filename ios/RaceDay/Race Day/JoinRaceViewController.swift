231852c80a1fdecf6cfe3601ee0bccd5459afc7c
Dick Fickling <dick@fickling.us>
2015-01-03 20:53:54 -0800

    Sync with couchbase

---
+++ ios/RaceDay/Race Day/JoinRaceViewController.swift
@@ -0,0 +1,39 @@
+//
+//  JoinRaceViewController.swift
+//  Race Day
+//
+//  Created by Dick Fickling on 1/3/15.
+//  Copyright (c) 2015 Questionable Intent. All rights reserved.
+//
+
+import UIKit
+
+class JoinRaceViewController: UIViewController {
+
+    var race: Race!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancelButtonPressed(sender: AnyObject) {
+        self.presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    @IBAction func okButtonPressed(sender: AnyObject) {
+        var db = (UIApplication.sharedApplication().delegate as AppDelegate).database
+        let doc = db.documentWithID("\(race.raceID)")
+        var props = doc.properties
+        var users = props["users"]! as [[String: AnyObject]]
+        users.append(["email": User.storedUser().email])
+        doc.putProperties(props, error: nil)
+        self.presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
--- ios/RaceDay/Race Day/User-extension.swift
+++ ios/RaceDay/Race Day/User-extension.swift
@@ -11,32 +11,86 @@ import Foundation
 extension User {
     
     class func enter(email: String, success: (User) -> (), failure: (NSError) -> ()) {
-        /*APIManager.post("/user", params: ["email": email],
-            success: { data in
-                let user = User(email: email)
-                return success(user)
-            },
-            failure: failure
-        )*/
         return success(User(email: email))
     }
     
     func getMyRaces(completion: ([Race], NSError?) -> ()) {
+        
+        let db = (UIApplication.sharedApplication().delegate as AppDelegate).database
+        
+        let query = db.viewNamed("viewByType").createQuery()
+        query.keys = ["race"]
+        
+        var error: NSError?
+        let result = query.run(&error)
+        
+        var myRaces: [Int] = []
+        while let row = result?.nextRow()?.value as? [String: AnyObject] {
+            if let users = row["users"] as? [[String: AnyObject]] {
+                for user in users {
+                    if let email = user["email"] as? String {
+                        if email == self.email {
+                            myRaces.append(row["esri_id"]! as Int)
+                        }
+                    }
+                }
+            }
+        }
+        
         if Races.sharedRaces().allRaces != nil {
-            return completion(Races.sharedRaces().allRaces as [Race], nil)
+            return completion(
+                (Races.sharedRaces().allRaces as [Race]).filter({ race in
+                    return (find(myRaces, Int(race.raceID)) != nil)
+                }
+                ), nil)
         } else {
             Races.sharedRaces().findRacesWithCompletion({ races, err in
-                return completion(races as [Race], err)
+                if err != nil { return completion([], err) }
+                return completion((races as [Race]).filter({ race in
+                    return (find(myRaces, Int(race.raceID)) != nil)
+                    }
+                    ), err)
             })
         }
     }
     
     func getNearbyRaces(completion: ([Race], NSError?) -> ()) {
+        
+        let db = (UIApplication.sharedApplication().delegate as AppDelegate).database
+        
+        let query = db.viewNamed("viewByType").createQuery()
+        query.keys = ["race"]
+        
+        var error: NSError?
+        let result = query.run(&error)
+        
+        var myRaces: [Int] = []
+        while let row = result?.nextRow()?.value as? [String: AnyObject] {
+            if let users = row["users"] as? [[String: AnyObject]] {
+                for user in users {
+                    if let email = user["email"] as? String {
+                        if email == self.email {
+                            myRaces.append(row["esri_id"]! as Int)
+                        }
+                    }
+                }
+            }
+        }
+        
+        
         if Races.sharedRaces().allRaces != nil {
-            return completion(Races.sharedRaces().allRaces as [Race], nil)
+            return completion(
+                (Races.sharedRaces().allRaces as [Race]).filter({ race in
+                    return (find(myRaces, Int(race.raceID)) == nil)
+                    }
+                ), nil)
         } else {
             Races.sharedRaces().findRacesWithCompletion({ races, err in
-                return completion(races as [Race], err)
+                if err != nil { return completion([], err) }
+                return completion((races as [Race]).filter({ race in
+                    return (find(myRaces, Int(race.raceID)) == nil)
+                    }
+                    ), err)
             })
         }
     }
