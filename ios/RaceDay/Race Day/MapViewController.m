bf312b53f2cc159caac10a5d744c4e0d436c899c
Scott Sirowy <ssirowy@gmail.com>
2015-01-03 21:45:20 -0800

    Pushing data to m2x

--- ios/RaceDay/Race Day/MapViewController.m
+++ ios/RaceDay/Race Day/MapViewController.m
@@ -28,17 +28,10 @@
 
 @property (nonatomic, assign) BOOL needToLoadRace;
 
-@property (nonatomic, strong) M2XClient* m2xClient;
-@property (nonatomic, strong) M2XDevice* device;
-@property (nonatomic, strong) M2XStream* stream;
-
 @end
 
 @implementation MapViewController
 
-
-#define kM2X_API_KEY @"7553e56ea444371828dd78ccf7ff58c8"
-#define kM2X_DEVICE_ID  @"2b31a57e778cbed2339ae9e34d4642f6"
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
@@ -63,45 +56,8 @@
     [self.mapView addMapLayer:tiledLayer withName:@"Basemap"];
     
     _geofenceLayer = [AGSGraphicsLayer graphicsLayer];
-    
-    /*  test */
-    
-    _m2xClient = [[M2XClient alloc] initWithApiKey:kM2X_API_KEY];
-    _device = [[M2XDevice alloc] initWithClient:self.m2xClient
-                                     attributes:@{@"id": kM2X_DEVICE_ID}
-               ];
-    
-    NSString* streamName = @"scottsirowy1";
-    
-    
-    NSDictionary* units = @{@"label": @"speed", @"symbol": @"miles/hr"};
-    __weak MapViewController* weakSelf = self;
-    [self.device updateStreamWithName:streamName
-                           parameters:@{@"unit": units, @"type": @"numeric"}
-                    completionHandler:^(M2XStream* s, M2XResponse* r) {
-                        weakSelf.stream = s;
-                        [weakSelf updateStream];
-                    }];
 }
      
-- (void)updateStream
-{
-    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-    NSLocale *enUSPOSIXLocale = [NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"];
-    [dateFormatter setLocale:enUSPOSIXLocale];
-    [dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZZZZZ"];
-    
-    NSDate *now = [NSDate date];
-    NSString *iso8601String = [dateFormatter stringFromDate:now];
-    NSLog(@"%@", iso8601String);
-    
-    [self.stream updateValue:[NSNumber numberWithFloat:54.0f]
-                   timestamp:iso8601String
-           completionHandler:^(M2XResponse* response){
-               NSLog(@"Something");
-           }];
-}
-
 - (void)startRace
 {
     [self.currentRace startRaceSimulatedSpeed:RaceSimulatedSpeedFast];
